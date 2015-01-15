a7ad4dafbe6824f5e32b8749db930dc50c9daa91
awp1121 <awpollon@gmail.com>
2015-01-14 21:53:37 -0500

    fixed class name in pom file

--- src/Editor.java
+++ src/Editor.java
@@ -78,7 +78,7 @@ public class Editor {
 	public static void main(String[] args) {
 		System.setProperty("apple.laf.useScreenMenuBar", "true");
 		System.setProperty("com.apple.mrj.application.apple.menu.about.name", "Holiday LX Editor");
-
+		
 		//Welcome screen
 		//		Welcome w = new Welcome();
 		boolean success = false;
